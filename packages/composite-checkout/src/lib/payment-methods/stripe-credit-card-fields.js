c2fdc51a4db1248cb400f1648fb37a0809b8fb9a
Payton Swick <payton@foolord.com>
2020-05-26 15:58:56 -0400

    Add transactionRedirectUrl to useTransactionStatus

--- packages/composite-checkout/src/lib/payment-methods/stripe-credit-card-fields.js
+++ packages/composite-checkout/src/lib/payment-methods/stripe-credit-card-fields.js
@@ -481,7 +481,7 @@ function StripePayButton( { disabled, store, stripe, stripeConfiguration } ) {
 							}
 							if ( stripeResponse?.redirect_url ) {
 								debug( 'stripe transaction requires redirect' );
-								setTransactionRedirecting( stripeResponse );
+								setTransactionRedirecting( stripeResponse.redirect_url );
 								return;
 							}
 							debug( 'stripe transaction is successful' );
